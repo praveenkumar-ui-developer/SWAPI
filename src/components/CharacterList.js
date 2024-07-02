9d58136ee8606612bcf290849407b95a89f5dd00
Praveen kumar <PRAVEENKUMAR.MECH2022@GMAIL.COM>
2024-07-02 12:08:10 +0530

    color changed

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -49,7 +49,7 @@ const CharacterList = () => {
           onClick={() => setPage(page - 1)}
           disabled={page === 1}
           mr={2}
-          colorScheme="purple"
+          colorScheme="pink"
           variant="outline"
           
         />
@@ -59,7 +59,7 @@ const CharacterList = () => {
           onClick={() => setPage(page + 1)}
           disabled={characters.length < 10}
           ml={2}
-          colorScheme="purple"
+          colorScheme="pink"
           variant="outline"
         />
       </Flex>
@@ -79,7 +79,7 @@ const CharacterList = () => {
           onClick={() => setPage(page - 1)}
           disabled={page === 1}
           mr={2}
-          colorScheme="purple"
+          colorScheme="pink"
           variant="outline"
         />
         <Text mx={2} fontWeight="bold" color="purple.700">Page {page}</Text>
@@ -88,7 +88,7 @@ const CharacterList = () => {
           onClick={() => setPage(page + 1)}
           disabled={characters.length < 10}
           ml={2}
-          colorScheme="purple"
+          colorScheme="pink"
           variant="outline"
         />
       </Flex>
